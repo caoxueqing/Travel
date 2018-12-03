6f3d06db7355879b37965635b3ac9af23f6bfa4c
caoxueqing <caoxueqing@qq.com>
2018-12-03 17:33:52 +0830

    城市页面更新

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe600;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe600;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -55,6 +57,7 @@ export default {
       width 1.24rem
       float left
       text-align center
+      color #FFF
       .arrow-icon
         font-size .24rem
         margin-left -0.04rem
